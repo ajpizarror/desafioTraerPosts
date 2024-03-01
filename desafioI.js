13923b315fb6940dfe1214060b459485d5f04607
Alfonso <ajpizarror@gmail.com>
2024-03-01 16:56:54 -0300

    avanzado

--- desafioI.js
+++ desafioI.js
@@ -1,5 +1,23 @@
 let BASE_URL = "https://jsonplaceholder.typicode.com/posts/";
 
+const postData = document.getElementById("post-data");
+const btn = document.querySelector("button");
+
+btn.addEventListener("click", () => {
+  showPosts();
+});
+
+const showPosts = async () => {
+  let response = await getPosts();
+  let dataPost = "";
+  response.forEach(
+    (response) =>
+      (dataPost += `<ul><li><h3>${response.title}</h3><p>${response.body}</p></li></ul>
+  `)
+  );
+  postData.innerHTML = dataPost;
+};
+
 const getPosts = async (url = BASE_URL) => {
   try {
     let response = await fetch(url);
@@ -10,22 +28,3 @@ const getPosts = async (url = BASE_URL) => {
     console.error("Este es el error:", error);
   }
 };
-
-const postData = document.getElementById("post-data");
-
-const showPosts = async () => {
-  let response = await getPosts();
-  let data = "";
-  response.forEach(
-    (response) =>
-      (data += `<ul><li><h3>${response.title}</h3><p>${response.body}</p></li></ul>
-  `)
-  );
-  postData.innerHTML = data;
-};
-
-const btn = document.querySelector("button");
-
-btn.addEventListener("click", () => {
-  showPosts();
-});
