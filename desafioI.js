a76e87b87619ef2193a23706cf24b1521bf3c3be
Alfonso <ajpizarror@gmail.com>
2024-03-01 16:47:36 -0300

    terminando

--- desafioI.js
+++ desafioI.js
@@ -4,6 +4,7 @@ const getPosts = async (url = BASE_URL) => {
   try {
     let response = await fetch(url);
     let data = await response.json();
+    // console.log(data.map((titlePar) => (titlePar.title)))
     return data;
   } catch (error) {
     console.error("Este es el error:", error);
@@ -14,9 +15,13 @@ const postData = document.getElementById("post-data");
 
 const showPosts = async () => {
   let response = await getPosts();
-  let postList = "";
-  postList += `<ul><li>${response.title}</li></ul><p>${response.body}</p>`;
-  postData.innerHTML = postList;
+  let data = "";
+  response.forEach(
+    (response) =>
+      (data += `<ul><li><h3>${response.title}</h3><p>${response.body}</p></li></ul>
+  `)
+  );
+  postData.innerHTML = data;
 };
 
 const btn = document.querySelector("button");
